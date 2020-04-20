0e8bbbe2e9172ac83b802b98f540083bcfad29ad
Rajasa Savant (RJ) <rasavant@microsoft.com>
2020-04-19 19:50:29 -0700

    Updated README.md, terraform main.tf output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -43,3 +43,45 @@ output "kube_config" {
 output "host" {
   value = azurerm_kubernetes_cluster.aks_app.kube_config.0.host
 }
+
+# Outputs required for deploy.yaml
+
+output "eventhub_namespace_conn_string_base64encoded" {
+  value = base64encode(azurerm_eventhub_namespace.hubns.default_primary_connection_string) 
+}
+
+output "eventhub_namespace_name_base64encoded" {
+  value = base64encode(azurerm_eventhub_namespace.hubns.name)
+}
+
+output "eventhub_namespace_name" {
+  value = azurerm_eventhub_namespace.hubns.name
+}
+
+output "eventhub_consumergroup_name_base64encoded"{
+  value = base64encode(azurerm_eventhub_consumer_group.group_rcvr_topic.name)
+}
+
+output "eventhub_consumergroup_name"{
+  value = azurerm_eventhub_consumer_group.group_rcvr_topic.name
+}
+
+output "eventhub_name_base64encoded" {
+  value = base64encode(azurerm_eventhub.rcvr_topic.name)
+}
+
+output "eventhub_name" {
+  value = azurerm_eventhub.rcvr_topic.name
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg_keda.name
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks_app.name
+}
